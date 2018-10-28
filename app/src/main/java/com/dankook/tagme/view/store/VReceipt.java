b9bc96ba8ab750c0007ff55d041869a247d1835c
hello2727 <jihye7369@gmail.com>
2018-10-28 22:00:18 +0900

    no message

---
+++ app/src/main/java/com/dankook/tagme/view/store/VReceipt.java
@@ -0,0 +1,15 @@
+package com.dankook.tagme.view.store;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dankook.tagme.R;
+
+public class VReceipt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt);
+    }
+}
