c7b2076035e62a2bcba9cd38f65a560e03a3e5e5
hello2727 <jihye7369@gmail.com>
2018-10-30 22:02:27 +0900

    no message

--- app/src/main/java/com/dankook/tagme/view/store/VReceipt.java
+++ app/src/main/java/com/dankook/tagme/view/store/VReceipt.java
@@ -3,6 +3,8 @@ package com.dankook.tagme.view.store;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.dankook.tagme.R;
 
@@ -13,6 +15,9 @@ public class VReceipt extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt);
 
+        RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup1);
+        RadioButton rb = (RadioButton)findViewById(R.id.radioButton4);
         Button btn = findViewById(R.id.button);
+        Button btn2 = findViewById(R.id.button2);
     }
 }
