b9024dae85ad5d90bb15bd9b9c867a2d05723fa5
hello2727 <jihye7369@gmail.com>
2018-10-31 11:03:14 +0900

    no message

--- app/src/main/java/com/dankook/tagme/view/store/VReceipt.java
+++ app/src/main/java/com/dankook/tagme/view/store/VReceipt.java
@@ -3,8 +3,10 @@ package com.dankook.tagme.view.store;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.dankook.tagme.R;
 
@@ -15,6 +17,13 @@ public class VReceipt extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt);
 
+        ImageView iv = (ImageView)findViewById(R.id.imageView);
+        TextView tv = (TextView)findViewById(R.id.textView);
+        TextView tv2 = (TextView)findViewById(R.id.textView2);
+        TextView tv5 = (TextView)findViewById(R.id.textView5);
+        TextView tv7 = (TextView)findViewById(R.id.textView7);
+        TextView tv8 = (TextView)findViewById(R.id.textView8);
+        TextView tv9 = (TextView)findViewById(R.id.textView9);
         RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup1);
         RadioButton rb = (RadioButton)findViewById(R.id.radioButton4);
         Button btn = findViewById(R.id.button);
